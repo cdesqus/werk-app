35f9569ed66572192aeae2709583cf2ea511f1fa
Haryanto Jayusman <cdesqus@gmail.com>
2025-12-03 15:49:09 +0700

    add api config logging and prioritize env var

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const apiUrl = import.meta.env.VITE_API_URL || (isLocal ? 'http://localhost:5000/api' : 'https://api-werk.kaumtech.com/api');
+
+console.log('Werk App API Config:', {
+    hostname: window.location.hostname,
+    isLocal,
+    envApiUrl: import.meta.env.VITE_API_URL,
+    finalApiUrl: apiUrl
+});
+
 const api = axios.create({
-    baseURL: isLocal ? 'http://localhost:5000/api' : 'https://api-werk.kaumtech.com/api',
+    baseURL: apiUrl,
 });
 
 api.interceptors.request.use((config) => {
